e9c2d2694cbf9c1c5659924b078bab6bd5961057
Gustavo Costa <gustavo.costa@lojainterativa.com>
2023-07-21 21:38:16 -0300

    front do componente products

--- src/app/modules/homeScreen/components/products/products.component.ts
+++ src/app/modules/homeScreen/components/products/products.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
+  products = [
+    { name: 'Product 1', price: 100, description: 'test of description', image: 'assets/images/product-example.png' },
+    { name: 'Product 2', price: 200, description: 'test of description', image: 'assets/images/product-example.png' },
+    { name: 'Product 3', price: 300, description: 'test of description', image: 'assets/images/product-example.png' },
+  ];
+
 }
